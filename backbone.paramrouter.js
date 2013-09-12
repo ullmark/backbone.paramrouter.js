1057868a3d2b24d7e3d9f88b81a2fd7091306f5b
Markus Ullmark <ullmark@gmail.com>
2013-09-12 18:18:15 +0200

    started the plugin

---
+++ backbone.paramrouter.js
@@ -0,0 +1,23 @@
+
+(function() {
+
+  'use strict';
+
+  // Backbone ParamRouter
+  // --------------------
+
+
+  (function(factory) {
+    if(typeof define === 'function' && define.amd) {
+      define(['backbone'], factory);
+    } else {
+      factory(Backbone);
+    }
+  }(function(Backbone) {
+
+    // ### Plugin Code
+    
+
+  }));
+
+}());
